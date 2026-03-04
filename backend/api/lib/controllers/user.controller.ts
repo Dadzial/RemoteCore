0275b97b6897fedfcea818dd611fdddb1065bccd
Dadzial <duszakdamian034@gmail.com>
2026-03-04 22:19:41 +0100

    test path ping

--- backend/api/lib/controllers/user.controller.ts
+++ backend/api/lib/controllers/user.controller.ts
@@ -7,11 +7,28 @@ import TokenService from "../modules/services/token.service";
 import logger from "../utils/logger";
 
 class UserController implements Controller {
-    public path = 'api/user'
+    public path = '/api/user'
     public router = Router();
 
     private userService = new UserService();
     private passwordService = new PasswordService();
     private tokenService = new TokenService();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes() : void {
+        this.router.get(`${this.path}/ping`, this.pingServer);
+    }
+
+    private pingServer = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            return response.status(200).json({ status: 'ok' });
+        } catch (err: any) {
+            logger.error(`Ping error: ${err.message}`);
+            return next(err);
+        }
+    };
 }
 export default UserController ;
