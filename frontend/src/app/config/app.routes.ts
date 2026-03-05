b41b2d104b41f156ae564f0fa4d88987f0ead30a
Dadzial <duszakdamian034@gmail.com>
2026-03-05 21:23:41 +0100

    prepare for login page

--- frontend/src/app/config/app.routes.ts
+++ frontend/src/app/config/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('../pages/auth/auth-module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('../pages/dashboard/dashboard-module').then(m => m.DashboardModule)
+  }
+];
