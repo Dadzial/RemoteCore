545f4c79f1fea66ac8664fe46560a5ebf65f5b80
Dadzial <duszakdamian034@gmail.com>
2026-03-13 21:01:28 +0100

    add proxy trust

--- backend/api/lib/index.ts
+++ backend/api/lib/index.ts
@@ -1,8 +1,8 @@
 import App from './app';
 import UserController from "./controllers/user.controller";
 
-
 const app = new App();
+app.app.set('trust proxy', 1);
 
 const controllers = [
     new UserController()
