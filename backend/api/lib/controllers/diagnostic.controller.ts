54f5355fdeb71933fcf8886b3faff79987ca15dd
Dadzial <duszakdamian034@gmail.com>
2026-05-13 20:48:42 +0200

    init of diagnostic controller

---
+++ backend/api/lib/controllers/diagnostic.controller.ts
@@ -0,0 +1,17 @@
+import wsControllerInterface from "../interfaces/ws-controller.interface";
+import { Server, Socket } from "socket.io";
+import logger from "../utils/logger";
+import joi from "joi";
+
+class ConnectionController  implements wsControllerInterface {
+    public io: Server;
+
+    constructor(io: Server) {
+        this.io = io;
+    }
+
+    public initializeWebSocketHandler() : void {
+
+    }
+}
+export default ConnectionController;
