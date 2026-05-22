744eac508c8a54dfaf602c68621a9a81db8d6a1d
Dadzial <duszakdamian034@gmail.com>
2026-05-22 11:46:14 +0200

    fix refersh bug

--- backend/api/lib/index.ts
+++ backend/api/lib/index.ts
@@ -5,6 +5,8 @@ import ConnectionController from "./controllers/connection.controller";
 import GryoController from "./controllers/gryo.controller";
 import LidarController from "./controllers/lidar.controller";
 import DiagnosticController from "./controllers/diagnostic.controller";
+import express from 'express';
+import path from 'path';
 
 const app = new App();
 const io = app.getIo();
@@ -29,6 +31,15 @@ controllers.forEach(controller => {
 
 wsControllers.forEach(controller => {
     controller.initializeWebSocketHandler();
+});
+
+
+const frontendPath = path.join(__dirname, '../../../frontend/dist/frontend/browser');
+app.app.use(express.static(frontendPath));
+
+
+app.app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
 app.listen()
--- backend/api/lib/middlewares/auth.middleware.ts
+++ backend/api/lib/middlewares/auth.middleware.ts
@@ -2,22 +2,24 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { config } from '../config';
 import { IUser } from "../modules/models/user.model";
+import TokenModel from "../modules/schemas/token.schema";
 
-export const auth = (request: Request, response: Response, next: NextFunction) => {
+export const auth = async (request: Request, response: Response, next: NextFunction) => {
     let token = request.headers['x-access-token'] || request.headers['authorization'];
     if (token && typeof token === 'string') {
         if (token.startsWith('Bearer ')) {
             token = token.slice(7, token.length);
         }
         try {
-            jwt.verify(token, config.JwtSecret, (err, decoded) => {
-                if (err) {
-                    return response.status(400).send('Invalid token.');
-                }
-                const user: IUser = decoded as IUser;
-                next();
-                return;
-            });
+            const decoded = jwt.verify(token, config.JwtSecret) as IUser;
+
+            const tokenExists = await TokenModel.findOne({ value: token });
+            if (!tokenExists) {
+                return response.status(401).send('Session expired or logged out.');
+            }
+
+            request.body.user = decoded;
+            next();
         } catch (ex) {
             return response.status(400).send('Invalid token.');
         }
